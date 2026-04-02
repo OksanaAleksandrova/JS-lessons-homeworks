06602ead2b7b8ddcb46ad0c304a2b201709f448d
OksanaAleksandrova <ov.aleksandrova.ua@gmail.com>
2026-04-02 18:33:41 +0300

    add lesson1-2, lesson1-3

---
+++ Lesson-1/3-data_types/main1-3.js
@@ -0,0 +1,13 @@
+// string - строчный тип данных
+let a = 'check';
+// a = '5';
+// console.log(typeof a);
+console.log(typeof a, a); // оператор выводит тип данных и что в ней записано
+
+// number - числовой тип данных
+let b = 100;
+console.log(typeof b, b);
+
+// boolean - логический тип данных (для проверки верности чего-то)
+let c = true;
+console.log(c);
