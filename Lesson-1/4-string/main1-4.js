b51a06499a0b566d22a1bc0f9435d6ade84d5015
OksanaAleksandrova <ov.aleksandrova.ua@gmail.com>
2026-04-03 16:10:36 +0300

    add lesson1-4

---
+++ Lesson-1/4-string/main1-4.js
@@ -0,0 +1,14 @@
+let a = "'variable a'";
+let b = '"variable b"';
+console.log(a, b);
+
+let c = `variable c`;
+console.log(c);
+
+let greetings = 'Hello!'
+let intro = 'My name is my name Roxy.';
+let msg = greetings + ' ' + intro;
+console.log(msg);
+
+let msg2 = `${greetings} ${intro}`;
+console.log(msg2);
