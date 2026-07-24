18046f7644c61d84628cac98987c61fd152f27a2
OksanaAleksandrova <ov.aleksandrova.ua@gmail.com>
2026-07-24 15:45:26 +0300

    add lesson1-1 - lesson1-6

---
+++ Lessons/Lesson-1/5-numbers/main1-5.js
@@ -0,0 +1,20 @@
+let a = 100500;
+console.log(a);
+let b = 3.123;
+console.log(b);
+
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(10 / 20);
+console.log(11 % 3); //деление по модулю, остаётся остаток от деления, т.е. тут получается 2
+console.log(0.1 + 0.2);
+
+// let big = 123456789012345678901234567n
+let big = BigInt(123456789012345678901234567)
+console.log(big);
+
+let n1 = 100;
+let n2 = 200;
+console.log(n1 + n2);
+
---
+++ Lessons/Lesson-1/6-boolean/main1-6.js
@@ -0,0 +1,11 @@
+console.log(10 > 5);
+console.log(10 > 20);
+console.log(10 === 10);
+console.log(10 !== 10);
+
+console.log(5 == '5');
+
+console.log('one' === 'one');
+console.log('onetwo' > 'one');
+
+console.log(5 != '6');
