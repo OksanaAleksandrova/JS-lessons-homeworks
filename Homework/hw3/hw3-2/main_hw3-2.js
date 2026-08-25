0fed1f1f38f6ef56ddad6ed99b4943e993b26257
OksanaAleksandrova <ov.aleksandrova.ua@gmail.com>
2026-08-25 16:51:04 +0300

    add hw3

---
+++ Homework/hw3/hw3-2/main_hw3-2.js
@@ -0,0 +1,8 @@
+// #TYj7ncx
+//
+// – За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+for (let i = 0; i < 10; i++) {
+    // document.write('<div>Homework 3-2. Index ' + i + '</div>');
+    document.write(`<div>Homework 3-2. Index ${i}</div>`);
+}
