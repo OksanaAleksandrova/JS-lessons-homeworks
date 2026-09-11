ba891d9ce07012f4f32adae7f8f2503e67660ec6
OksanaAleksandrova <ov.aleksandrova.ua@gmail.com>
2026-09-11 11:03:08 +0300

    add homework4

--- Homework/hw4/hw4-12/main_hw4-12.js
+++ Homework/hw4/hw4-12/main_hw4-12.js
@@ -8,8 +8,7 @@ function swap(arr, index1, index2) {
     if (index1 >= 0 &&
         index2 >= 0 &&
         index1 < arr.length &&
-        index2 < arr.length)
-    {
+        index2 < arr.length) {
         let temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
